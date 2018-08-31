c6895fba3a978d6777f4d9ff11bb8751f42314d5
mikhail <mkv-1724@mail.ru>
2018-08-31 21:23:54 +0300

    Alter model: add IssueComment, Alter model Issue

--- bugtracker_app/models.py
+++ bugtracker_app/models.py
@@ -1,18 +1,21 @@
 import uuid
+from textwrap import shorten
 
 from django.db import models
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import ModelForm
 from django.utils.deconstruct import deconstructible
-from textwrap import shorten
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
 
 from . import models_default_values as mdv
 
-class SingletonModel(models.Model):
 
+class SingletonModel(models.Model):
     class Meta:
         abstract = True
 
@@ -61,22 +64,33 @@ class IssueImportance(models.Model):
 class Issue(models.Model):
     subject = models.CharField(max_length=50)
     description = models.TextField()
-    author_name = models.CharField(max_length=30)
-    author_email = models.EmailField()
     notify_by_email = models.BooleanField(default=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now_add=True)
+
+    # for anonymous users
+    author_name = models.CharField(max_length=30, null=True, blank=True)
+    author_email = models.EmailField(null=True, blank=True)
+
     # FK
+    author = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        null=True)
+
     status = models.ForeignKey(
         IssueStatus,
         on_delete=models.PROTECT,
         default=IssueStatus.get_default()[0])
+
     importance = models.ForeignKey(
         IssueImportance,
         on_delete=models.PROTECT)
+    
     area = models.ForeignKey(
         IssueArea,
         on_delete=models.PROTECT)
+
     category = models.ForeignKey(
         IssueCategory,
         on_delete=models.PROTECT,
@@ -89,7 +103,18 @@ class Issue(models.Model):
     def get_absolute_url(self):
         return reverse('bugtracker:issue_detail', args=(self.id,))
 
+class IssueComment(models.Model):
+    content = models.TextField()
+    support_user = models.CharField(max_length=255, null=True, blank=True)
+    end_user_email = models.EmailField(null=True, blank=True)
+    created_date = models.DateTimeField()
+    # FK
+    issue = models.ForeignKey(
+        Issue,
+        on_delete=models.PROTECT)
+
 class Setting(SingletonModel):
+    #REDMINE SETTINGS 
     redmine_url = models.CharField(max_length=255, default=mdv.redmine_url)
     redmine_api_access_key = models.CharField(max_length=100)
     project_id = models.CharField(max_length=255)
@@ -98,16 +123,15 @@ class Setting(SingletonModel):
         help_text='Как часто будет происходить синхронизация с redmine'
     )
 
+    #EMAIL SETTINGS 
     smtp_email_client_host = models.CharField(default=mdv.smtp_host, max_length=200)
     smtp_email_client_port = models.PositiveIntegerField(default=mdv.smtp_port)
-
-    imap_email_client_host = models.CharField(default=mdv.smtp_host, max_length=200)
-    imap_email_client_port = models.PositiveIntegerField(default=mdv.smtp_port)
-
+    imap_email_client_host = models.CharField(default=mdv.imap_host, max_length=200)
+    imap_email_client_port = models.PositiveIntegerField(default=mdv.imap_port)
     email_login = models.CharField(max_length=255)
     email_password = models.CharField(max_length=255)
 
-    api_key = models.UUIDField(default=uuid.uuid4, editable=False)
+    api_key = models.UUIDField(default=uuid.uuid4)
 
     def __str__(self):
         return 'Setting'
