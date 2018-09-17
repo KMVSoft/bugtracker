77a6d21373687a0aa2e945d2e3d824cd71df2270
mikhail <mkv-1724@mail.ru>
2018-09-17 07:05:41 +0300

    added comments in file models_default_values.py

--- bugtracker_app/models_default_values.py
+++ bugtracker_app/models_default_values.py
@@ -1,7 +1,8 @@
-
+//тут хранятся значения по умолчанию
+//для моделей
 status_name = 'новая'
 smtp_host = 'smtp.gmail.com'
 smtp_port = 465
 imap_host = 'imap.google.com'
 imap_port = 993
-redmine_url='http://locahost:3000'
+redmine_url='http://locahost:3000'`
