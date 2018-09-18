f3ed03e0e9ae0bc0ada0c2d3ed7f8e756c20ac33
mikhail <mkv-1724@mail.ru>
2018-09-18 21:55:31 +0300

    deleted serializer

--- bugtracker_app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import IssueComment
-
-class IssueCommentSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = IssueComment
-		fields = ()
