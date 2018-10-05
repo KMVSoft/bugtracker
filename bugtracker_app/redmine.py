7b3f7df985cc555c3b0523e24e96469b7e7d32be
mikhail <mkv-1724@mail.ru>
2018-10-05 09:43:32 +0300

    added comments in redmine.py

--- bugtracker_app/redmine.py
+++ bugtracker_app/redmine.py
@@ -4,8 +4,25 @@ from .models import Setting
 
 setting = Setting.load()
 
+def get_id_of_custom_field(name:str):
+    '''Получает id custom_field по его имени (name), в случае ошибки
+    Возвращает отрицательное число, если не удалось сделать это'''
+    r = requests.get(
+        setting.redmine_url+'/custom_fields.json',
+        params={'key':setting.redmine_api_access_key}
+    )
+    if r.status_code != 200:
+        return -1;
+    for field in r.json()['custom_fields']:
+        if field['name'] == name:
+            return field['id']
+    return -2;
 
 def create_issue(issue):
+    '''Создаёт новую задачу в redmine, на вход передаётся объект модели задачи,
+    если всё прошло удачно, возвращает созданный объект
+    иначе вызывает исключение, вызывать в блоке try/except Exception as e'''
+    get_id_of_custom_field('user_name')
     r = requests.post(
             setting.redmine_url+'/issues.json',
             json={
@@ -13,9 +30,15 @@ def create_issue(issue):
                     "subject": issue.subject,
                     "description": issue.description,
                     "custom_fields":[
-                        {"value": issue.author_email, "name": "user_email", "id": 1},
-                        {"value": issue.author_name, "name": "user_name", "id": 2},
-                        {"value": issue.area.name, "name": "issue_area", "id": 3},
+                        {"value": issue.author_email,
+                         "name": "user_email",
+                         "id": get_id_of_custom_field('user_email')},
+                        {"value": issue.author_name,
+                         "name": "user_name", 
+                         "id": get_id_of_custom_field('user_name')},
+                        {"value": issue.area.name,
+                         "name": "issue_area", 
+                         "id": get_id_of_custom_field('issue_area')},
                     ],
                     "priority_id": issue.importance.priority,
                 },
@@ -26,6 +49,9 @@ def create_issue(issue):
     return r.json()
 
 def create_note(issue_id:int, note:str):
+    '''Создаёт новое замечание с текстом note,
+     в журнале задачи c идентификатором issue_id,
+     возвращает response запроса'''
     r = requests.put(
             setting.redmine_url+'/issues/%d.json' % issue_id,
             json={
@@ -38,6 +64,9 @@ def create_note(issue_id:int, note:str):
     return r
 
 def get_issues_list():
+    '''Возвращает список всех задач, которые есть в redmine
+    если всё прошло удачно, возвращает список задач,
+    иначе вызывает исключение, вызывать в блоке try/except Exception as e'''
 	r = requests.get(
 		setting.redmine_url+'/issues.json',
 		params={'key':setting.redmine_api_access_key,
@@ -47,6 +76,9 @@ def get_issues_list():
 	return r.json()
 
 def get_issue(id:int):
+    '''Возвращает данные задачи из redmine по её идентификатору id
+    если всё прошло удачно, возвращает данные задачи,
+    иначе вызывает исключение, вызывать в блоке try/except Exception as e'''
 	r = requests.get(
 		setting.redmine_url+'/issues/%d.json' % id,
 		params={'key':setting.redmine_api_access_key}		
