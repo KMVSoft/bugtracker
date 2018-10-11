cb7f437e6ca80ed4cae6a2b1d9f46f20c0b8a615
mikhail <mkv-1724@mail.ru>
2018-10-11 20:05:47 +0300

    add new url path

--- bugtracker/urls.py
+++ bugtracker/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bugs/', include('bugtracker_app.urls')),
+    path('/', include('bugtracker_app.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
